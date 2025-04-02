c6cbb7076071cbb2365bf56ecf02e1cd93fa87b4
kakhmadov <kakhmadov@student.tgm.ac.at>
2025-04-02 21:32:11 +0200

    Wrote the method contents.

--- code/src/model/GenericStack.java
+++ code/src/model/GenericStack.java
@@ -3,27 +3,34 @@ package model;
 public class GenericStack<E> {
     private E[] elements;
     private int size;
+    private int top = 0;
 
     public GenericStack() {
         elements = (E[]) new Object[10];
-        size = 10;
+        size=10;
     }
 
     public GenericStack(int size)   {
         elements = (E[]) new Object[size];
-        this.size = size;
+        this.size=size;
     }
 
     public void push(E element) {
-
+        if (top == size) throw new StackFullException();
+        else elements[top++] = element;
     }
 
     public E pop() {
-        return null;
+        if(top==0) throw new StackEmptyException();
+        else {
+            elements[top--]=null;
+            return elements[top];
+        }
     }
 
     public E peek() {
-        return null;
+        if(top==0) throw new StackEmptyException();
+        else return elements[top];
     }
 
     public String list()    {
