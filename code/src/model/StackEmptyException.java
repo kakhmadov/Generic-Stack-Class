08f47ab2d412c470baeff6df427afd1432d8e5a5
kakhmadov <kakhmadov@student.tgm.ac.at>
2025-04-02 21:31:14 +0200

    Added default constructor.

--- code/src/model/StackEmptyException.java
+++ code/src/model/StackEmptyException.java
@@ -1,6 +1,7 @@
 package model;
 
 public class StackEmptyException extends RuntimeException {
+    public StackEmptyException() {super("Stack is empty");}
     public StackEmptyException(String message) {
         super(message);
     }
--- code/src/model/StackFullException.java
+++ code/src/model/StackFullException.java
@@ -1,6 +1,7 @@
 package model;
 
 public class StackFullException extends RuntimeException {
+    public StackFullException() {super("Stack is full");}
     public StackFullException(String message) {
         super(message);
     }
