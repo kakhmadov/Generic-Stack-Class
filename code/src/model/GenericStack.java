95853b91dd9d040b2bb68b2216ae7350b2fac355
kakhmadov <kakhmadov@student.tgm.ac.at>
2025-04-02 19:58:04 +0200

    Added attributes and methods, as well as constructors.

--- code/src/model/GenericStack.java
+++ code/src/model/GenericStack.java
@@ -1,4 +1,33 @@
 package model;
 
-public class GenericStack {
+public class GenericStack<E> {
+    private E[] elements;
+    private int size;
+
+    public GenericStack() {
+        elements = (E[]) new Object[10];
+        size = 10;
+    }
+
+    public GenericStack(int size)   {
+        elements = (E[]) new Object[size];
+        this.size = size;
+    }
+
+    public void push(E element) {
+
+    }
+
+    public E pop() {
+        return null;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public String list()    {
+        String list="";
+        return list;
+    }
 }
