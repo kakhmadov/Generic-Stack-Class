c865819d3c88f828ca3702262dbf18245fd731d8
kakhmadov <kakhmadov@student.tgm.ac.at>
2025-04-03 10:58:17 +0200

    Fixed functional mistakes in the methods and wrote some test code in the Test.java class. I also added documentation comments for the methods in the GenericStack.java class.

--- code/src/model/GenericStack.java
+++ code/src/model/GenericStack.java
@@ -1,40 +1,96 @@
 package model;
 
+import java.util.EmptyStackException;
+
+/**
+ * This class demonstrates a generic stack class.
+ * @author Akhmadov Khasu
+ * @version 2025-04-03
+ * @param <E> A type-parameter which enables the usage of generics.
+ */
 public class GenericStack<E> {
     private E[] elements;
     private int size;
     private int top = 0;
 
+    /**
+     * Default constructor, which creates a Generic-Array with
+     * the size 10.
+     */
     public GenericStack() {
         elements = (E[]) new Object[10];
         size=10;
     }
 
+    /**
+     * Constructor with the parameter "size", to determine the
+     * size of the Generic-Array
+     * @param size An integer value to set the amount of elements
+     *             for the Generic-Array.
+     */
     public GenericStack(int size)   {
         elements = (E[]) new Object[size];
         this.size=size;
     }
 
-    public void push(E element) {
+    /**
+     * This method allows to push a new element into the
+     * Generic-Array. The elements are stacked on top of
+     * each other.
+     * @param element An element that is to be added to the
+     *                Generic-Array.
+     * @throws StackFullException Is thrown, when the programm tries
+     * to add a new element into a full Generic-Stack.
+     */
+    public void push(E element) throws StackFullException {
         if (top == size) throw new StackFullException();
         else elements[top++] = element;
     }
 
-    public E pop() {
+    /**
+     * This method removes the element at the very top of the
+     * Generic-Array. It functions by the stack principle.
+     * @return Returns the last removed element from the Generic-Array.
+     * @throws StackEmptyException Is thrown, when the programm tries to remove
+     * an element from an empty Generic-Array.
+     */
+    public E pop() throws StackEmptyException {
         if(top==0) throw new StackEmptyException();
         else {
-            elements[top--]=null;
-            return elements[top];
+            E r=elements[top-1];
+            elements[top-1]=null;
+            top-=1;
+            return r;
         }
     }
 
-    public E peek() {
+    /**
+     * This method allows the programm to return the very top element in a
+     * Generic-Array.
+     * @return Returns the very top element from a Generic-Array.
+     * @throws StackEmptyException Is thrown, when the programm tries to return
+     * the very top element from an empty Generic-Array.
+     */
+    public E peek() throws StackEmptyException {
         if(top==0) throw new StackEmptyException();
-        else return elements[top];
+        else return elements[top-1];
     }
 
+    /**
+     * This method concatenates the elements from the Generic-Array, this method
+     * is applied to, as a String and returns it.
+     * @return Returns all the elements from the Generic-Array as a string.
+     */
     public String list()    {
         String list="";
+        if(top!=0) {
+            for (E e : elements)
+                if (e!=null || top!=size) {
+                    list = list + e+";";
+                }
+                else return list;
+        }
+        else throw new StackEmptyException();
         return list;
     }
 }
--- code/src/model/Test.java
+++ code/src/model/Test.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Test {
+    public static void main(String[] args) {
+        GenericStack<String> stack1 = new GenericStack<>();
+        GenericStack<Integer> stack2 = new GenericStack<>();
+
+        stack1.push("a");
+        stack1.push("b");
+        stack1.push("c");
+        stack1.push("d");
+
+        stack2.push(1);
+        stack2.push(2);
+        stack2.push(3);
+        stack2.push(4);
+
+        System.out.println("Stack 1: " + stack1.list()+"\nStack 2: " + stack2.list());
+        System.out.println("\nStack1 peek: " + stack1.peek()+"\nStack1 pop: " + stack1.pop());
+        System.out.println("Stack2 peek: " + stack2.peek()+"\nStack2 pop: " + stack2.pop());
+        System.out.println("\nStack 1: " + stack1.list()+"\nStack 2: " + stack2.list());
+        System.out.println("\nStack1 peek: " + stack1.peek()+"\nStack2 peek: " + stack2.peek());
+
+        stack1.push("e");
+        stack1.push("f");
+        stack1.push("g");
+        stack1.push("h");
+        stack1.push("i");
+        stack1.push("j");
+        stack1.push("k");
+
+        System.out.println("\nStack 1: " + stack1.list());
+
+        //The code below triggers the StackFullException for the stack1:
+        // stack1.push("l");
+
+        GenericStack<Integer> stack3 = new GenericStack<>();
+        //The code below triggers the StackEmptyException for the stack3:
+        //stack3.pop();
+    }
 }
